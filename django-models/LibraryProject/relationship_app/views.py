9bb921bdf96bdc5261369b7859cf6a052010441b
yasser116926 <yasserbasher222@gmail.com>
2025-12-05 01:22:20 +0300

    Convert nested repo into normal folder

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
 
-# --- Function-Based View ---
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
-
-# --- Class-Based View ---
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "library_detail.html"
-    context_object_name = "library"
+# Create your views here.
