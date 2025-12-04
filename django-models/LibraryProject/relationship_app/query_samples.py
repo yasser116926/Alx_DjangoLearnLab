55fb41f0d08cbffff3bb153034839bf68ccc095d
yasser116926 <yasserbasher222@gmail.com>
2025-12-05 00:39:28 +0300

    Convert nested repo into normal folder

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,54 +1,19 @@
-from relationship_app.models import Author, Book, Library
+from .models import Author, Book, Library, Librarian
 
 
-def get_all_books():
-    """Return all books."""
-    return Book.objects.all()
-
-
-def get_books_by_author(author_name):
-    """Return books written by a specific author."""
-    return Book.objects.filter(author__name=author_name)
-
-
+# 1. Get a library by name
 def get_library_by_name(library_name):
-    """Return a single library object by its name."""
-    # THIS is the line they said is missing
     return Library.objects.get(name=library_name)
 
 
-def get_books_in_library(library_name):
-    """Return all books in a specific library."""
+# 2. Get books written by a specific author
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
+
+
+# 3. Retrieve the librarian for a given library
+def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
-
-
-def create_author(name):
-    """Create a new author."""
-    author = Author.objects.create(name=name)
-    return author
-
-
-def create_book(title, publication_year, author_name):
-    """Create a book and link it to an author."""
-    author, created = Author.objects.get_or_create(name=author_name)
-    book = Book.objects.create(
-        title=title,
-        publication_year=publication_year,
-        author=author
-    )
-    return book
-
-
-def create_library(name):
-    """Create a new library."""
-    library = Library.objects.create(name=name)
-    return library
-
-
-def add_book_to_library(book_title, library_name):
-    """Add an existing book to the library."""
-    book = Book.objects.get(title=book_title)
-    library = Library.objects.get(name=library_name)
-    library.books.add(book)
-    return library
+    # Assuming Library has OneToOneField or ForeignKey to Librarian
+    return library.librarian
