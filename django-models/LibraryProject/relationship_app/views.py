5e6292a78d5bb2bb80fbc393a109f7682f005c3b
yasser116926 <yasserbasher222@gmail.com>
2025-12-05 01:16:16 +0300

    Convert nested repo into normal folder

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
+from django.shortcuts import render
 from django.views.generic import DetailView
-from django.http import HttpResponse
+from .models import Book, Library
 
-
-# Function-based view
+# --- Function-Based View ---
 def list_books(request):
     books = Book.objects.all()
     return render(request, "list_books.html", {"books": books})
 
+# --- Class-Based View ---
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "library_detail.html"
     context_object_name = "library"
-
-def home(request):
-    return HttpResponse("Welcome , this is the homepage ")    
--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,36 +1,12 @@
-"""
-URL configuration for LibraryProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from relationship_app.views import home
 from django.shortcuts import redirect
 
 def redirect_to_books(request):
     return redirect('list_books')
 
 urlpatterns = [
-    path('', redirect_to_books),
-    path('admin/', admin.site.urls),
-    path('books/', include('relationship_app.urls')),
-]
-
-
-urlpatterns = [
-    path('', home, name='home'),  # <--- THIS FIXES THE 404
+    path('', redirect_to_books, name='root_redirect'),  
     path('admin/', admin.site.urls),
     path('books/', include('relationship_app.urls')),
 ]
