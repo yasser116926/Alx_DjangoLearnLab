e972e44d5165c2d5c71fb07e0721cd9feff88fcb
yasser116926 <yasserbasher222@gmail.com>
2025-12-05 07:27:16 +0300

    Convert nested repo into normal folder

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
+from .models import Library
 
 # --- ADD THIS ---
 def list_books(request):
