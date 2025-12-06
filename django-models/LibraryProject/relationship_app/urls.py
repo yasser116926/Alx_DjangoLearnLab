c1b1ed7766af9532878813140a68e49d6df3734d
yasser116926 <yasserbasher222@gmail.com>
2025-12-06 04:12:00 +0300

    Add auth: register view, login/logout views & templates, urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,11 +3,13 @@ from . import views
 from .views import list_books, LibraryDetailView
 from .views import UserLoginView, UserLogoutView, registe
 
+app_name = 'relationship_app'
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+
 ]
