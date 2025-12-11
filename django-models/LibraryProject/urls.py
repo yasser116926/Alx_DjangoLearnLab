f339d73c701e9daf5157c308a2b5a34b60c41185
yasser116926 <yasserbasher222@gmail.com>
2025-12-11 03:48:39 +0300

    convert nested repo into normal folder

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -22,4 +22,10 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('', include('relationship_app.urls')),
+
+
+
+    path('books/',add_book, name='add_book'),
+    path('books/',edit_book, name='edit_book'),
+    path('books/',delete_book, name='delete_book'),
 ]
